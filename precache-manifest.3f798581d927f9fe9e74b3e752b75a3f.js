5441f7841c6672d63697359465c9a5a73728ef8e
Lake <lake@peardeck.com>
2019-09-04 09:44:34 -0400

    Updates

---
+++ precache-manifest.3f798581d927f9fe9e74b3e752b75a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af741c370b0b5572ce03ceee6ca5e11",
+    "url": "/project-coco/index.html"
+  },
+  {
+    "revision": "d8a1beb3fe2dddd59aaf",
+    "url": "/project-coco/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3875ecaf98c681cac083",
+    "url": "/project-coco/static/js/2.23244aec.chunk.js"
+  },
+  {
+    "revision": "d8a1beb3fe2dddd59aaf",
+    "url": "/project-coco/static/js/main.d730077a.chunk.js"
+  },
+  {
+    "revision": "37f547adf29741172c9e",
+    "url": "/project-coco/static/js/runtime~main.bae52f53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-coco/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a35644784db570e15ad7f9cf4a4de7f.js"
+  "/project-coco/precache-manifest.3f798581d927f9fe9e74b3e752b75a3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-coco/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
