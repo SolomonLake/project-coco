c2eb3eaa7f3af912a5a9685cb800579f2210d39a
Lake <lake@peardeck.com>
2019-09-04 23:48:56 -0400

    moved react components and started on zoom oauth

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
+
+const zoomAuthQueryParams = `response_type=code&client_id=HqnmSZ3VTuWHG7X4DuEh9A&redirect_uri=${window.location.href}`;
+
+window.open(`https://zoom.us/oauth/authorize?${zoomAuthQueryParams}`, "_self");
 
 ReactDOM.render(<App />, document.getElementById("root"));
