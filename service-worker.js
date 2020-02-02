1aef07fa3b995a86e7171df8ab2d03c974b47be3
SolomonLake <lakegh@gmail.com>
2020-02-02 13:45:05 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.252e9558e9619eee919ed2be90ee96d4.js"
+  "./precache-manifest.55fd0376470645cb984c3b2159c58382.js"
 );
 
 self.addEventListener('message', (event) => {
